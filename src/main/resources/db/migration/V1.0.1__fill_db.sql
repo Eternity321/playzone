d9080bae1238cd7c5ae1e366728849b758c67dbd
Evgeniy <j.akimushkin2003@gmail.com>
2025-05-30 03:21:42 +0300

    hot fix

--- src/main/resources/db/migration/V1.0.1__fill_db.sql
+++ src/main/resources/db/migration/V1.0.1__fill_db.sql
@@ -22,21 +22,7 @@ VALUES ('Футбол'),
        ('Теннис'),
        ('Настольный теннис'),
        ('Плавание'),
-       ('Мотокросс');
-
-INSERT INTO sport_facility (name, address, latitude, longitude, description, sport_type_id, created_by)
-VALUES ('Стадион "Юность"', 'ул. Спортивная, 12', 55.7558, 37.6176, 'Современный стадион с натуральным газоном',
-        (SELECT sport_type_id FROM sport_type WHERE name = 'Футбол'),
-        (SELECT users_id FROM users WHERE username = 'admin')),
-
-       ('Баскет-холл', 'ул. Олимпийская, 5', 55.7512, 37.6189, 'Закрытая площадка для баскетбола с трибунами',
-        (SELECT sport_type_id FROM sport_type WHERE name = 'Баскетбол'),
-        (SELECT users_id FROM users WHERE username = 'admin')),
-
-       ('Теннис-клуб "ACE"', 'пр. Спортивный, 7', 55.7520, 37.6160, 'Открытые и крытые теннисные корты',
-        (SELECT sport_type_id FROM sport_type WHERE name = 'Теннис'),
-        (SELECT users_id FROM users WHERE username = 'admin')),
-
-       ('Трасса для мотокросса', 'ул. Здоровья, 3', 55.7599, 37.6201, 'Поле с трассой для мотокросса',
-        (SELECT sport_type_id FROM sport_type WHERE name = 'Мотокросс'),
-        (SELECT users_id FROM users WHERE username = 'admin'));
+       ('Мотокросс'),
+       ('Гольф'),
+       ('Бадминтон'),
+       ('Хоккей');
